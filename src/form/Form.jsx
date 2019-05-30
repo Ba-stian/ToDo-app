5896d25a0144270179a7e6321a9870c19a7b94d3
bastian <k.batakovich@gmail.com>
2019-05-30 11:47:11 +0300

    Create form component

---
+++ src/form/Form.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Button from '../button/Button.jsx';
+import form from './Form.module.css';
+
+class Form extends React.Component {
+    state = {
+        content: ''
+    };
+    render() {
+        return(
+            <div className={form.wrap}>
+                <h2 className={form.header}>Add new task:</h2>
+                <form className={form.form} onSubmit={this.onSubmit}>
+                    <input className={form.input} type="text" onChange={this.onChange} value={this.state.content}/>
+                    <Button className={form.btn}>add</Button>
+                </form>
+            </div>
+        )
+    }
+
+    onChange = (e) => {
+        this.setState({
+            content: e.target.value
+        })
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+           this.props.addTodo(this.state);
+        this.setState({
+            content: ''
+        });
+    }
+}
+
+export default Form;
